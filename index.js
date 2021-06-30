07eabfbb812310fbf2a713449e12e11ed81ca05d
Cristian <cristiangomez.mail@gmail.com>
2021-06-30 10:46:38 -0300

    se modifica index.js del server para agregar rutas al cliente 2

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const app = express();
 
 if (process.env.NODE_ENV === "production") {
   //set static folder
-  app.use(express.static("client"));
+  app.use(express.static("client/public"));
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
 });
 
 //habilita cors
